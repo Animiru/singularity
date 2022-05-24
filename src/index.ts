71c64b468155a7c633e58810ce18f7e95519e1cd
Nobu <chat@nobu.sh>
2022-05-24 18:32:57 -0500

    fix version execution scope issue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-export const version = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf-8')).version
+export const version = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'package.json'), 'utf-8')).version
 export * from './Pr_Ipv4'
 export * from './Mid'
 export * as Util from './utils/creation'
